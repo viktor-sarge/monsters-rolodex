66bb6820039b64055a9b7950c933679548236e97
Viktor Sarge <viktor.sarge@regionhalland.se>
2022-10-16 13:22:56 +0300

    feat: ✨ add start of app
    Påbörjat appen

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchField: ''
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response)=>response.json())
+      .then((data) => this.setState(
+        () => {return {monsters: data, filteredMonsters: data};}
+      ));
+  }
+
+  render() {
+    const filteredMonsters = this.state.monsters.filter(
+      monster => monster.name.toLocaleLowerCase().includes(this.state.searchField)
+    );
+
+    return (
+    <div className="App"> 
+      <input className='search-box' type='search' placeholder='Monster name' onChange={(event)=>{
+        const searchField = event.target.value.toLocaleLowerCase();
+        this.setState( () => {
+          return { searchField }
+        }
+        );
+      }} />
+      {filteredMonsters.map((monster)=>{
+        return <div key={monster.id}><h1>{monster.name}</h1></div>})}
     </div>
-  );
+  ); 
+
+  }
 }
 
 export default App;
